671c66738e9fd9d84e1c8cedc2a0a353f7243b3a
AndresDBF <andres200605@gmail.com>
2024-06-18 11:36:32 -0400

    webhook4

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, jsonify
+""" from flask import Flask, request, render_template, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import json
@@ -77,4 +77,84 @@ def recibir_mensajes(req):
     return jsonify({'message':'EVENT_RECEIVED'})
 
 if __name__=='__main__':
-    app.run(host='0.0.0.0', port=80,debug=True)
+    app.run(host='0.0.0.0', port=80,debug=True) """
+
+from flask import Flask, request, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import http.client
+import json
+
+app = Flask(__name__)
+
+#Configuracion de la base de datos SQLITE
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///metapython.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db =SQLAlchemy(app)
+
+#Modelo de la tabla log
+class Log(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    fecha_y_hora = db.Column(db.DateTime, default=datetime.utcnow)
+    texto = db.Column(db.TEXT)
+
+#Crear la tabla si no existe
+with app.app_context():
+    db.create_all()
+
+#Funcion para ordenar los registros por fecha y hora
+def ordenar_por_fecha_y_hora(registros):
+    return sorted(registros, key=lambda x: x.fecha_y_hora,reverse=True)
+
+@app.route('/')
+def index():
+    #obtener todos los registros ed la base de datos
+    registros = Log.query.all()
+    registros_ordenados = ordenar_por_fecha_y_hora(registros)
+    return render_template('index.html',registros=registros_ordenados)
+
+mensajes_log = []
+
+#Funcion para agregar mensajes y guardar en la base de datos
+def agregar_mensajes_log(texto):
+    mensajes_log.append(texto)
+    print("mensaje texto: ", texto)
+    #Guardar el mensaje en la base de datos
+    nuevo_registro = Log(texto=texto)
+    print("nuevo registro: ", nuevo_registro)
+    db.session.add(nuevo_registro)
+    db.session.commit()
+
+#Token de verificacion para la configuracion
+TOKEN_ANDERCODE = "ANDERCODE"
+
+@app.route('/webhook', methods=['GET','POST'])
+def webhook():
+    if request.method == 'GET':
+        challenge = verificar_token(request)
+        return challenge
+    elif request.method == 'POST':
+        reponse = recibir_mensajes(request)
+        return reponse
+
+def verificar_token(req):
+    token = req.args.get('hub.verify_token')
+    challenge = req.args.get('hub.challenge')
+
+    if challenge and token == TOKEN_ANDERCODE:
+        return challenge
+    else:
+        return jsonify({'error':'Token Invalido'}),401
+
+def recibir_mensajes(req):
+    print(req)
+    req = request.get_json()
+    print(req)
+    agregar_mensajes_log(req)
+    
+    return jsonify({'message':'EVENT_RECEIVED'})
+
+ 
+
+if __name__=='__main__':
+    app.run(host='0.0.0.0',port=80,debug=True)
